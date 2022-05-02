47c38b7e6c1542409f9203db5878636820fa0624
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-05-02 13:54:02 +0200

    update

--- js/blog.js
+++ js/blog.js
@@ -1,5 +1,5 @@
 const baseUrl =
-  "https://mayth.one/project-exam/wp-json/wp/v2/destinations?acf_format=standard";
+  "https://mayth.one/project-exam/wp-json/wp/v2/destinations?per_page=12&acf_format=standard";
 const postContainer = document.querySelector(".post-container");
 
 async function getPosts(url) {
@@ -11,7 +11,7 @@ async function getPosts(url) {
 
     posts.forEach(function (post) {
       postContainer.innerHTML += `<div class="post">   
-                                    <div style="background-image: linear-gradient(rgba(0 ,0,0,0), rgba(0,0,0,0.7)), url(${post.acf.featured_image})" class="featured-img">
+                                    <div style="background-image: linear-gradient(rgba(0 ,0,0,0), rgba(0,0,0,0.5)), url(${post.acf.featured_image})" class="featured-img">
                                     <h2>${post.title.rendered}</h2>
                                     <p>${post.excerpt.rendered}</p>
                                     <a href="post.html?id=${post.id}">Read More</a>
